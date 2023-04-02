b45df721589cf397032fc15c0d5de544b66751b6
JJQuiet <jjquiet@qq.com>
2023-04-03 00:32:42 +0800

    ✨ feat(修改gitignore): 修改gitignore
    修改gitignore
    
    修改gitignore

--- t.ps1
+++
@@ -1,9 +0,0 @@
-# npm i
-# npm i axios
-# yarn add @umijs/plugin-sass
-npm start
-start http://localhost:8000/jjqlogin
-# http://localhost:8001/user/login
-# start http://localhost:8557
-# git init
-# git commit -m 1.0.1-pro complete初始化及添加自定义login
